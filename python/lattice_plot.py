c8e112290134ea333ce2ec1afd30c06b61df7bbe
Zhentao Wang <wztzjhn@gmail.com>
2019-02-09 16:53:15 -0700

    add triangular lattice 31-site-cluster

--- python/lattice_plot.py
+++ python/lattice_plot.py
@@ -50,34 +50,44 @@ for sub in range(0,num_sub):
     sites.append(np.array(site_temp))
     carts.append(np.array(cart_temp))
 
+xmin = np.amin(carts[0][:,0])
+xmax = np.amax(carts[0][:,0])
+ymin = np.amin(carts[0][:,1])
+ymax = np.amax(carts[0][:,1])
+for sub in range(1,num_sub):
+    if np.amin(carts[sub][:,0]) < xmin:
+        xmin = np.amin(carts[sub][:,0])
+    if np.amax(carts[sub][:,0]) > xmax:
+        xmas = np.amax(carts[sub][:,0])
+    if np.amin(carts[sub][:,1]) < ymin:
+        ymin = np.amin(carts[sub][:,1])
+    if np.amax(carts[sub][:,1]) > ymax:
+        ymax = np.amax(carts[sub][:,1])
 
-xmin = np.minimum(np.amin(carts[0][:,0]),np.amin(carts[1][:,0]))
-xmax = np.maximum(np.amax(carts[0][:,0]),np.amax(carts[1][:,0]))
-ymin = np.minimum(np.amin(carts[0][:,1]),np.amin(carts[1][:,1]))
-ymax = np.maximum(np.amax(carts[0][:,1]),np.amax(carts[1][:,1]))
 
-plt.plot(carts[0][:,0],carts[0][:,1],"ro")
-plt.plot(carts[1][:,0],carts[1][:,1],"bs")
+plotstyle = [ "o", "s", "^", "v", "*" ]
+plotcolor = [ "r", "b", "m", "g", "c"]
+for sub in range(0,num_sub):
+    plt.plot(carts[sub][:,0],carts[sub][:,1],plotstyle[sub],color=plotcolor[sub])
 
-plt.plot(carts[0][:,0]+(R[0][0]*a[0]+R[0][1]*a[1])[0],
-         carts[0][:,1]+(R[0][0]*a[0]+R[0][1]*a[1])[1],'o',color='orange')
-plt.plot(carts[1][:,0]+(R[0][0]*a[0]+R[0][1]*a[1])[0],
-         carts[1][:,1]+(R[0][0]*a[0]+R[0][1]*a[1])[1],"s",color='orange')
-plt.plot(carts[0][:,0]+(R[1][0]*a[0]+R[1][1]*a[1])[0],
-         carts[0][:,1]+(R[1][0]*a[0]+R[1][1]*a[1])[1],'o',color='grey')
-plt.plot(carts[1][:,0]+(R[1][0]*a[0]+R[1][1]*a[1])[0],
-         carts[1][:,1]+(R[1][0]*a[0]+R[1][1]*a[1])[1],"s",color='grey')
+for sub in range(0,num_sub):
+    plt.plot(carts[sub][:,0]+(R[0][0]*a[0]+R[0][1]*a[1])[0],
+             carts[sub][:,1]+(R[0][0]*a[0]+R[0][1]*a[1])[1],plotstyle[sub],color='orange')
+    plt.plot(carts[sub][:,0]+(R[1][0]*a[0]+R[1][1]*a[1])[0],
+             carts[sub][:,1]+(R[1][0]*a[0]+R[1][1]*a[1])[1],plotstyle[sub],color='grey')
 
 #plt.xlim(math.floor(xmin-0.7),math.ceil(xmax+0.7))
 #plt.ylim(math.floor(ymin-0.7),math.ceil(ymax+0.7))
 plt.axes().set_aspect('equal')
 
-plt.arrow(0,0,a[0][0],a[0][1],color='magenta')
-plt.arrow(0,0,a[1][0],a[1][1],color='green')
-plt.arrow(a[0][0],a[0][1],a[1][0],a[1][1],color='green')
-plt.arrow(a[1][0],a[1][1],a[0][0],a[0][1],color='magenta')
+plt.arrow(0,0,a[0][0],a[0][1],color='black')
+plt.arrow(0,0,a[1][0],a[1][1],color='black')
+plt.arrow(a[0][0],a[0][1],a[1][0],a[1][1],color='black')
+plt.arrow(a[1][0],a[1][1],a[0][0],a[0][1],color='black')
 
 plt.arrow(0,0,(R[0][0]*a[0]+R[0][1]*a[1])[0],(R[0][0]*a[0]+R[0][1]*a[1])[1],color='orange')
 plt.arrow(0,0,(R[1][0]*a[0]+R[1][1]*a[1])[0],(R[1][0]*a[0]+R[1][1]*a[1])[1],color='grey')
+
+plt.title("total sites: "+str(len(sites[0])*num_sub))
 
 plt.show()
